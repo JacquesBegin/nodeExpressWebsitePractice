cedbeff2a84f69b9c6d1dc62cc7bf748aea2fb73
Jacques Begin <jacques.andre.begin@gmail.com>
2019-02-23 17:08:06 -0500

    The error view, error css, and route to error view.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const createError = require('http-errors');
 const path = require('path');
 
 const app = express();
@@ -16,6 +17,20 @@ app.get('/favicon.ico', (req, res, next) => {
 });
 
 app.use('/', routes());
+
+app.use((req, res, next) => {
+  return next(createError(404, 'File not found'));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(status);
+  res.locals.pizza = 'pepperoniPizza';
+  return res.render('error');
+});
 
 app.listen(3000);
 
